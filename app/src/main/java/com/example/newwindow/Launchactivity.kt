82fec6771862951600ae78fd3f15c837f1f6ffcb
boryaxray <alek7ey2003@gmail.com>
2025-05-23 11:50:01 +0500

    first commit

---
+++ app/src/main/java/com/example/newwindow/Launchactivity.kt
@@ -0,0 +1,28 @@
+package com.example.newwindow
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class Launchactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launchactivity)
+        val timer = object : CountDownTimer(  3000, 1000){
+            override fun onTick(millisUnitFinished: Long){
+
+            }
+
+            override fun onFinish() {
+                val intent= Intent(this@Launchactivity,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+
+
+        }
+        timer
+    }
+}
--- app/src/main/java/com/example/newwindow/MainActivity.kt
+++ app/src/main/java/com/example/newwindow/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.newwindow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-    }
+
+}
 }
